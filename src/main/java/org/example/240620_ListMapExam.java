3f9032ebbf6264e4efdc57fb9ec510886d6781ad
jby9725 <jby9725@gmail.com>
2024-06-20 16:41:47 +0900

    ListMapExam.java add

---
+++ src/main/java/org/example/240620_ListMapExam.java
@@ -0,0 +1,39 @@
+//package org.example;
+//
+//import java.util.ArrayList;
+//import java.util.HashMap;
+//import java.util.List;
+//import java.util.Map;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        // 선언
+//        // (제네릭된) 리스트
+//        List<Integer> ages = new ArrayList<>();
+//        // (제네릭된) 맵
+//        Map<String, Integer> ages2 = new HashMap<>();
+//
+//        // 요소 추가
+//        // 리스트
+//        ages.add(20);
+//        ages.add(30);
+//        ages.add(40);
+//        // 맵
+//        ages2.put("John", 20);
+//        ages2.put("Jane", 30);
+//        ages2.put("Jack", 40);
+//
+//        // 요소 조회
+//        // 리스트
+//        System.out.println("철수 나이 : " + ages.get(0));
+//        System.out.println("영희 나이 : " + ages.get(1));
+//        System.out.println("영수 나이 : " + ages.get(2));
+//        // 맵
+//        System.out.println("Jone's age : " + ages2.get("John"));
+//        System.out.println("Jane's age : " + ages2.get("Jane"));
+//        System.out.println("Jack's age : " + ages2.get("Jack"));
+//
+//
+//    }
+//}
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,97 +1,39 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        v4();
-    }
 
-    static void v4() {
-        System.out.println("==v4==");
-        List<Article> articles = new ArrayList<>();
+        // 선언
+        // (제네릭된) 리스트
+        List<Integer> ages = new ArrayList<>();
+        // (제네릭된) 맵
+        Map<String, Integer> ages2 = new HashMap<>();
 
-        articles.add(new Article()); // 0
-        articles.add(new Article()); // 1
-        articles.add(new Article()); // 2
-        articles.add(new Article()); // 3
+        // 요소 추가
+        // 리스트
+        ages.add(20);
+        ages.add(30);
+        ages.add(40);
+        // 맵
+        ages2.put("John", 20);
+        ages2.put("Jane", 30);
+        ages2.put("Jack", 40);
+
+        // 요소 조회
+        // 리스트
+        System.out.println("철수 나이 : " + ages.get(0));
+        System.out.println("영희 나이 : " + ages.get(1));
+        System.out.println("영수 나이 : " + ages.get(2));
+        // 맵
+        System.out.println("Jone's age : " + ages2.get("John"));
+        System.out.println("Jane's age : " + ages2.get("Jane"));
+        System.out.println("Jack's age : " + ages2.get("Jack"));
 
 
-        for (int i = 0; i < articles.size(); i++) {
-            Article article = articles.get(i);
-            System.out.println(article.id);
-        }
-    }
-
-    static void v3() {
-        System.out.println("==v3==");
-        ArrayList articles = new ArrayList();
-
-        articles.add(new Article()); // 0
-        articles.add(new Article()); // 1
-        articles.add(new Article()); // 2
-        articles.add(new Article()); // 3
-
-        for (int i = 0; i < articles.size(); i++) {
-            Article article = (Article) articles.get(i);
-            System.out.println(article.id);
-        }
-    }
-
-    static void v2() {
-        System.out.println("==v2==");
-        ArrayList articles = new ArrayList();
-
-        articles.add(1); // 0
-        articles.add(new Article()); // 1
-        articles.add(new Article()); // 2
-        articles.add(new Article()); // 3
-
-        for (int i = 0; i < articles.size(); i++) {
-            Article article = (Article) articles.get(i);
-            System.out.println(article.id);
-        }
-    }
-
-    static void v1() {
-        System.out.println("==v1==");
-        Article[] articles = new Article[100];
-
-        int articlesSize = 0;
-
-        articles[0] = new Article();
-        articlesSize++;
-        articles[1] = new Article();
-        articlesSize++;
-        articles[2] = new Article();
-        articlesSize++;
-        articles[3] = new Article();
-        articlesSize++;
-
-        for (int i = 0; i < articlesSize; i++) {
-            System.out.println(articles[i].id);
-        }
-    }
-
-}
-
-class Article {
-    static int lastId;
-    int id;
-    String regDate;
-
-    static {
-        lastId = 0;
-    }
-
-    Article() {
-        this(lastId + 1, "2024-12-12 12:12:12");
-        lastId++;
-    }
-
-    Article(int id, String regDate) {
-        this.id = id;
-        this.regDate = regDate;
     }
 }
